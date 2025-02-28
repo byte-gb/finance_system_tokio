73a7c5d501124fe30939279afb272a5831163c86
Gabriel <gabrielbarreto60@hotmail.com>
2025-02-28 02:06:29 -0300

    dev. controller do projeto

--- backend/src/main/java/tokiomarine/finance_system/controller/TransferenciaController.java
+++ backend/src/main/java/tokiomarine/finance_system/controller/TransferenciaController.java
@@ -1,4 +1,40 @@
 package tokiomarine.finance_system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tokiomarine.finance_system.model.Transferencia;
+import tokiomarine.finance_system.repository.TransferenciaRepository;
+import tokiomarine.finance_system.service.TransferenciaService;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/transferencias")
 public class TransferenciaController {
+    @Autowired
+    private TransferenciaRepository transferenciaRepository;
+
+    @Autowired
+    private TransferenciaService transferenciaService;
+
+    @PostMapping("/agendar")
+    public ResponseEntity<?> agendarTransferencia(@Valid @RequestBody Transferencia transferencia) {
+        try {
+            BigDecimal taxa;
+            taxa = transferenciaService.calcularTaxa(transferencia.getDataTransferencia(), transferencia.getValor());
+            transferencia.setTaxa(taxa);
+            transferencia.setDataAgendamento(LocalDate.now());
+            transferenciaRepository.save(transferencia);
+
+            return ResponseEntity.ok("Transferência agendada com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
+        }
+    }
 }
