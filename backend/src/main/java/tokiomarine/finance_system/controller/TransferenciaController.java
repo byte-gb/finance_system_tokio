ccdf24ffe768b162ab75794c379a3050447e905d
Gabriel <gabrielbarreto60@hotmail.com>
2025-02-28 12:46:41 -0300

    Ajuste CORS error ao enviar request

--- backend/src/main/java/tokiomarine/finance_system/controller/TransferenciaController.java
+++ backend/src/main/java/tokiomarine/finance_system/controller/TransferenciaController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/transferencias")
+@CrossOrigin
 public class TransferenciaController {
     @Autowired
     private TransferenciaRepository transferenciaRepository;
